3640ee29839c790d4ef41c5a57fece3c89094cab
Mahya $ Sina <mahyaahmadi00@gmail.com>
2024-04-25 18:50:34 +0330

    sim_card uploaded

--- model/entity/sim_card.py
+++ model/entity/sim_card.py
@@ -3,7 +3,7 @@ from model.entity.base import Base
 
 class SimCard(Base):
     def __init__(self, number, operator, price, owner):
-        self.SimCard_id = None
+        self.sim_card_id = None
         self.number = number
         self.operator = operator
         self.price = price
