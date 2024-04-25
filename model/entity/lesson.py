0a422c164ad781b3a550d0c4c31da0359eae0ec3
Arash <Arasholoomi@outlook.com>
2024-04-25 17:05:49 +0330

    Digram Uploaded

---
+++ model/entity/lesson.py
@@ -0,0 +1,10 @@
+from model.entity.base import Base
+
+
+class Lesson(Base):
+    def __init__(self, name, grade, teacher, start_day):
+        self.lesson_id = None
+        self.name = name
+        self.grade = grade
+        self.teacher = teacher
+        self.start_day = start_day
