621a020a63d063f15f1f608fe133d8ecfa673ef8
HafezAghili <amirhosseinhafezaghili@gmail.com>
2024-04-25 20:22:33 +0330

    lesson test v3.1

--- test/lesson_test.py
+++ test/lesson_test.py
@@ -12,23 +12,23 @@ l_controller = LessonController()
 # status, message = l_controller.remove(1)
 
 # TEST PASSED , find_all
-status, message = l_controller.find_all()
+# status, message = l_controller.find_all()
 
 # TEST PASSED , find_by_id
 # status, message = l_controller.find_by_id(2)
 
 # TEST PASSED , find_by_name
-# status, lesson_list = l_controller.find_by_name("math")
+# status, message = l_controller.find_by_name("math")
 
 # TEST PASSED , find_by_teacher
-# status, lesson_list = l_controller.find_by_name("Hafez")
+# status, message = l_controller.find_by_name("Hafez")
 
 
-if status:
-    # msg.showinfo()
-    print("Info")
-    print(message)
-else:
-    # msg.showerror()
-    print("Error")
-    print(message)
+# if status:
+#     # msg.showinfo()
+#     print("Info")
+#     print(message)
+# else:
+#     # msg.showerror()
+#     print("Error")
+#     print(message)
