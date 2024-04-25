62795b48a1bc865e8d7764a4b217eed5811930b2
Messbah <Messbah.A@Gmail.com>
2024-04-25 19:47:43 +0330

    updated

--- model/tools/validator.py
+++ model/tools/validator.py
@@ -38,3 +38,28 @@ class Validator:
             return grade
         else:
             raise ValueError(message)
+
+
+    def Number_Validator(self, number, message):
+        if re.match(r"^[0-9]{2,11}$", number):
+            return number
+        else:
+            raise ValueError(message)
+
+    def Operator_Validator(self, operator, message):
+        if re.match(r"^[a-zA-Z\s]{2,20}$", operator):
+            return operator
+        else:
+            raise ValueError(message)
+
+    def Price_Validator(self, price, message):
+        if re.match(r"^[0-9]{2,11}$", price):
+            return price
+        else:
+            raise ValueError(message)
+
+    def Owner_Validator(self, owner, message):
+        if re.match(r"^[a-zA-Z\s]{5,50}$", owner):
+            return owner
+        else:
+            raise ValueError(message)
