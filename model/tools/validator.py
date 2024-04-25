e0e70d9ccbb853e32fd1121e02d0a029a76f7d87
Messbah <Messbah.A@Gmail.com>
2024-04-25 18:49:31 +0330

    validator updated

--- model/tools/validator.py
+++ model/tools/validator.py
@@ -15,6 +15,19 @@ class Validator:
         except:
             raise ValueError(message)
 
+    def date_time_validator(self, year, month, day, hour, minute, second, message):
+        try:
+            return datetime(year, month, day, hour, minute, second)
+        except:
+            raise ValueError(message)
+
+    def amount_validator(self, amount, message):
+        if amount > 0:
+            return amount
+        else:
+            raise ValueError(message)
+
+
     def grade_validator(self, grade, message):
         if re.match(r"^[a-zA-Z\s]{2,15}$", grade):
             return grade
