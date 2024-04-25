10b652d8e1ac6c384bcd52c461612d072a90d504
Mahya $ Sina <mahyaahmadi00@gmail.com>
2024-04-25 19:31:32 +0330

    sim_card uploaded

--- model/tools/sim_card_validator.py
+++ model/tools/sim_card_validator.py
@@ -2,25 +2,25 @@ import re
 
 
 class SimCard_Validator:
-    def number_validator(self, number, message):
+    def Number_Validator(self, number, message):
         if re.match(r"^[0-9]{2,11}$", number):
             return number
         else:
             raise ValueError(message)
 
-    def operator_validator(self, operator, message):
+    def Operator_Validator(self, operator, message):
         if re.match(r"^[a-zA-Z\s]{2,20}$", operator):
             return operator
         else:
             raise ValueError(message)
 
-    def price_validator(self, price, message):
+    def Price_Validator(self, price, message):
         if re.match(r"^[0-9]{2,11}$", price):
             return price
         else:
             raise ValueError(message)
 
-    def owner_validator(self, owner, message):
+    def Owner_Validator(self, owner, message):
         if re.match(r"^[a-zA-Z\s]{5,50}$", owner):
             return owner
         else:
