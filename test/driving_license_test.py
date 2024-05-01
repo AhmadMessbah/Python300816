12a94df3812cca7ada2c86f41060c103cfa0d67f
amirrezanalchi <amirreza@gmail.com>
2024-05-01 21:25:33 +0330

    add driving license test and view file

---
+++ test/driving_license_test.py
@@ -0,0 +1,32 @@
+from controller.DrivingLicense_controller import DrivingLicenseController
+
+drivingLicenseController = DrivingLicenseController()
+
+# TEST PASSED
+# status, message =  drivingLicenseController.save("1234","2024-01-02","tehran","2025-01-04")
+
+# TEST PASSED
+# status, message =  drivingLicenseController.edit(1,"1444","2024-02-02","kish","2027-01-04")
+
+# TEST PASSED
+# status, message =  drivingLicenseController.remove(1)
+
+# TEST PASSED
+# status, drivingLicenseList = drivingLicenseController.find_all()
+
+# TEST PASSED
+# status, drivingLicense = drivingLicenseController.find_by_id(1)
+
+# TEST PASSED
+# status, drivingLicense = drivingLicenseController.find_by_serial_number("1234")
+
+
+
+# if status:
+#     # msg.showinfo()
+#     print("Info")
+#     print(message)
+# else:
+#     # msg.showerror()
+#     print("Error")
+#     print(message)
