e5c246dabb89a90e2618c9d814ab806b4529e3fb
Messbah.A <Messbah.A@Gmail.com>
2024-05-02 17:36:09 +0330

    Messbah Updated

--- model/tools/decorators.py
+++ model/tools/decorators.py
@@ -5,10 +5,11 @@ def exception_handling(function):
     def inner(*args, **kwargs):
         try:
             output = function(*args, **kwargs)
-            Logger.info(f"Function {function.__name__} - {args[1:]} returned {output[1]}")
+            Logger.info(f"Function {function.__qualname__} - {args[1:]} returned {output[1]}")
             return output
         except Exception as e:
             Logger.error(f"Function - {args, kwargs} Has Error {e}")
             return False, str(e)
 
     return inner
+
