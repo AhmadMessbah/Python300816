47b727daf60a9c4a8a4877baca4385d37cc9c84f
Arash <arasholoomi@outlook.com>
2024-05-02 17:37:39 +0330

    Controller + entity (Lesson) Uploaded

--- test/lesson_test.py
+++ test/lesson_test.py
@@ -1,24 +1,24 @@
 from controller.lesson_controller import LessonController
 
 # # TEST PASSED , save
-# status, message = LessonController.save("aaa", "vvv", "Oloomi", 2021, 5, 25)
+# status, message = LessonController.save("Windows", "elementary", "Oloomi", 2021, 5, 25)
 
 # TEST PASSED , edit
-status, message = LessonController.edit(4, "math", "elementary", "Hafez", 2021, 5, 25)
+# status, message = LessonController.edit(6, "math", "elementary", "Hafez", 2021, 5, 10)
 
 # TEST PASSED , remove
-# status, message = LessonController.remove(4)
+# status, message = LessonController.remove(8)
 
 # TEST PASSED , find_all
-# status, lesson_list = l_controller.find_all()
+# status, lesson_list = LessonController.find_all()
 
 # TEST PASSED , find_by_id
-# status, lesson = l_controller.find_by_id(20)
+# status, lesson = LessonController.find_by_id(6)
 
 # TEST PASSED , find_by_name
-# status, lesson_list = l_controller.find_by_name("python")
+# status, lesson_list = LessonController.find_by_name("python")
 
 # TEST PASSED , find_by_teacher
-# status, lesson_list = l_controller.find_by_teacher("oloomi")
+# status, lesson_list = LessonController.find_by_teacher("oloomi")
 
 # #
