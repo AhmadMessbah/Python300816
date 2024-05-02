31c28470f5164274f7145a0cc317d28e4fc29ea5
Messbah.A <Messbah.A@Gmail.com>
2024-05-02 16:56:02 +0330

    Messbah Updated

--- model/da/db.sql
+++ model/da/db.sql
@@ -75,3 +75,12 @@ create table driving_license_tbl (
     city varchar(255),
     expire_date timestamp
 );
+
+create table medical_record(
+    id int primary key auto_increment,
+    disease nvarchar(20) not null ,
+    medicine nvarchar(20) not null ,
+    datee date,
+    doctor nvarchar(20),
+    status bool not null
+);
