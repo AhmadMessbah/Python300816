662c5c87e491d8c25e04decc0f4e0e70f0655e75
Arash <arasholoomi@outlook.com>
2024-05-02 16:52:12 +0330

    Controller + entity (Lesson) Uploaded

--- model/entity/lesson.py
+++ model/entity/lesson.py
@@ -1,4 +1,5 @@
 from model.entity.base import Base
+from model.tools.validator import Validator
 
 
 class Lesson(Base):
@@ -8,3 +9,41 @@ class Lesson(Base):
         self.grade = grade
         self.teacher = teacher
         self.start_day = start_day
+
+
+def get_name(self):
+    return self._name
+
+
+def set_name(self, name):
+    self._name = Validator.name_validator(name, "Invalid Lesson Name")
+
+
+def get_grade(self):
+    return self._grade
+
+
+def set_grade(self, grade):
+    self._grade = grade
+
+
+def get_teacher(self):
+    return self._teacher
+
+
+def set_teacher(self, teacher):
+    self._teacher = Validator.name_validator(teacher, "Invalid Teacher Name")
+
+
+def get_start_day(self):
+    return self._start_day
+
+
+def set_start_day(self, start_day):
+    self._start_day = Validator.date_validator(start_day, "Invalid Date")
+
+
+name = property(get_name, set_name)
+family = property(get_grade, set_grade)
+grade = property(get_grade, set_grade)
+start_day = property(get_start_day, set_start_day)
