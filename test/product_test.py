c8bd8953683ecccf098799488de520474ba3d954
Amin babaei <007icyamin007@gmail.com>
2024-05-02 18:02:21 +0330

    product updated

--- test/product_test.py
+++ test/product_test.py
@@ -1,19 +1,19 @@
 from controller.product_controller import ProductController
-product_controller = ProductController()
 
 
-
-# TEST PASSED
-# status , message = product_controller.save("iphonx","Apple","jx1616","1000")
-# status , message = product_controller.save("a51","samsung","lk9988","1500")
 
 
 # TEST PASSED
-# status , message = product_controller.edit("1","A51","samsung","XD568","2000")
+# status , message = ProductController.save("iphonx","Apple","jx1616","1000")
+# status , message = ProductController.save("a51","samsung","lk9988","1500")
+
 
 # TEST PASSED
-# status, message = product_controller.remove(1)
+# status , message = ProductController.edit("4","A51","samsung12","XD568","2000")
 
 # TEST PASSED
-# status,message = person_list = product_controller.find_all()
-status, message = product_controller.find_by_id(40)
+status, message = ProductController.remove(4)
+
+# TEST PASSED
+# status,message = person_list = ProductController.find_all()
+# status, message = ProductController.find_by_id(40)
