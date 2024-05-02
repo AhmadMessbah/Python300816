12b0bac600626488a1f5109e55cbac0462022bed
mohamadrezanikmaram <mohamadrezanikmaram7@gmail.com>
2024-05-02 17:47:12 +0330

    controller updated

--- controller/financial_doc_controller.py
+++ controller/financial_doc_controller.py
@@ -5,13 +5,13 @@ from model.entity.financial_doc import FinancialDoc
 from model.tools.decorators import exception_handling
 
 
-class FinancialDoc_Controller:
-    financial_doc_da = Financial_Doc_Da
+class FinancialDocController:
+    financial_doc_da = Financial_Doc_Da()
 
     @classmethod
     @exception_handling
     def save(cls, amount, doc_type, description):
-        financial_doc = FinancialDoc(amount,doc_type,description)
+        financial_doc = FinancialDoc(amount, datetime.now(), doc_type, description)
         cls.financial_doc_da.save(financial_doc)
         return True, f"financial_doc saved successfully\n{financial_doc}"
 
@@ -40,8 +40,6 @@ class FinancialDoc_Controller:
     @exception_handling
     def find_by_id(cls, financial_doc_id):
         return True, cls.financial_doc_da.find_by_id(financial_doc_id)
-
-
 
     @classmethod
     @exception_handling
