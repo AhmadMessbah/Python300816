9ec3d5947aa6bd6a7fd8a2bfd91255def434d25d
HafezAghili <amirhosseinhafezaghili@gmail.com>
2024-05-09 16:21:09 +0330

    update/change lesson_tbl

--- model/da/db.sql
+++ model/da/db.sql
@@ -14,8 +14,9 @@ create table lesson_tbl
     id        int primary key auto_increment,
     name      nvarchar(30) not null,
     grade     nvarchar(15) not null,
-    teacher   nvarchar(30) not null,
-    start_day date
+    teacher_id   int,
+    start_day date,
+    foreign key (teacher_id) references mft.person_tbl(id)
 );
 
 -- Product
