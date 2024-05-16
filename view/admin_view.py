b9d4e66f9956dd50de361256c296c19d23317d20
Messbah <Messbah.A@Gmail.com>
2024-05-16 19:31:03 +0330

    Jalase 10- Updated - messbah
    Driving License
    Financial Document
    Search By Product ID
    Search By SimCard Number
    
    ------------------------
    
    Admin Panel
    Access Level

---
+++ view/admin_view.py
@@ -0,0 +1,89 @@
+from tkinter import *
+import tkinter.messagebox as msg
+
+from controller.person_controller import PersonController
+from view.component.label_text import TextWithLabel
+from view.component.table import Table
+from view.main_view import MainView
+
+
+class PersonView:
+    def reset_form(self):
+        self.id.variable.set("")
+        self.name.variable.set("")
+        self.family.variable.set("")
+        status, person_list = PersonController.find_all()
+        if status:
+            self.table.refresh_table(person_list)
+
+    def select_row(self,person):
+        self.id.variable.set(person[0])
+        self.name.variable.set(person[1])
+        self.family.variable.set(person[2])
+
+    def save_click(self):
+        status, message = PersonController.save(self.name.variable.get(), self.family.variable.get())
+        if status:
+            msg.showinfo("Save Person", "Person Saved")
+            self.reset_form()
+        else:
+            msg.showerror("Save Error", message)
+
+    def edit_click(self):
+        status, message = PersonController.edit(self.id.variable.get(), self.name.variable.get(), self.family.variable.get())
+        if status:
+            msg.showinfo("Edit Person", "Person Edited")
+            self.reset_form()
+        else:
+            msg.showerror("Edit Error", message)
+
+    def remove_click(self):
+        status, message = PersonController.remove(self.id.variable.get())
+        if status:
+            msg.showinfo("Remove Person", "Person Removed")
+            self.reset_form()
+        else:
+            msg.showerror("Remove Error", message)
+
+    def find_by_family(self,event):
+        status, person_list = PersonController.find_by_family(self.search_family.variable.get())
+        if status:
+            self.table.refresh_table(person_list)
+
+    # todo :add this function
+    def close_win(self):
+        self.win.destroy()
+        main_view = MainView(self.user)
+
+    def __init__(self, user):
+        self.user = user
+        self.win = Tk()
+
+        # todo : add these two line
+        Label(text=user.person.name + " " + user.person.family).place(x=0, y=0)
+        self.win.protocol("WM_DELETE_WINDOW", self.close_win)
+
+        self.win.geometry("600x300")
+        self.win.title("Person")
+
+        self.id = TextWithLabel(self.win, "Id", 20, 20, disabled=True)
+        self.name = TextWithLabel(self.win, "Name", 20, 60)
+        self.family = TextWithLabel(self.win, "Family", 20, 100)
+        self.search_family = TextWithLabel(self.win, "Family", 300, 270)
+        self.search_family.text_box.bind("<KeyRelease>", self.find_by_family)
+
+        self.table = Table(self.win,
+                      ["Id", "Name", "Family"],
+                      [60, 100, 100],
+                      250,
+                      20,
+                      self.select_row)
+
+        Button(self.win, text="Add", width=8, command=self.save_click).place(x=20, y=250)
+        Button(self.win, text="Edit", width=8, command=self.edit_click).place(x=100, y=250)
+        Button(self.win, text="Remove", width=8, command=self.remove_click).place(x=180, y=250)
+
+        self.reset_form()
+
+        self.win.mainloop()
+
--- view/person_view.py
+++ view/person_view.py
@@ -9,25 +9,10 @@ from view.main_view import MainView
 
 class PersonView:
     def reset_form(self):
-        self.id.variable.set("")
-        self.name.variable.set("")
-        self.family.variable.set("")
-        status, person_list = PersonController.find_all()
-        if status:
-            self.table.refresh_table(person_list)
+        self.id.variable.set(self.user.person.person_id)
+        self.name.variable.set(self.user.person.name)
+        self.family.variable.set(self.user.person.family)
 
-    def select_row(self,person):
-        self.id.variable.set(person[0])
-        self.name.variable.set(person[1])
-        self.family.variable.set(person[2])
-
-    def save_click(self):
-        status, message = PersonController.save(self.name.variable.get(), self.family.variable.get())
-        if status:
-            msg.showinfo("Save Person", "Person Saved")
-            self.reset_form()
-        else:
-            msg.showerror("Save Error", message)
 
     def edit_click(self):
         status, message = PersonController.edit(self.id.variable.get(), self.name.variable.get(), self.family.variable.get())
@@ -37,20 +22,7 @@ class PersonView:
         else:
             msg.showerror("Edit Error", message)
 
-    def remove_click(self):
-        status, message = PersonController.remove(self.id.variable.get())
-        if status:
-            msg.showinfo("Remove Person", "Person Removed")
-            self.reset_form()
-        else:
-            msg.showerror("Remove Error", message)
 
-    def find_by_family(self,event):
-        status, person_list = PersonController.find_by_family(self.search_family.variable.get())
-        if status:
-            self.table.refresh_table(person_list)
-
-    # todo :add this function
     def close_win(self):
         self.win.destroy()
         main_view = MainView(self.user)
@@ -59,29 +31,17 @@ class PersonView:
         self.user = user
         self.win = Tk()
 
-        # todo : add these two line
-        Label(text=user.person.name + " " + user.person.family).place(x=0, y=0)
+        # Label(text=user.person.name + " " + user.person.family).place(x=0, y=0)
         self.win.protocol("WM_DELETE_WINDOW", self.close_win)
 
-        self.win.geometry("600x300")
-        self.win.title("Person")
+        self.win.geometry("250x250")
+        self.win.title("Profile Person")
 
         self.id = TextWithLabel(self.win, "Id", 20, 20, disabled=True)
         self.name = TextWithLabel(self.win, "Name", 20, 60)
         self.family = TextWithLabel(self.win, "Family", 20, 100)
-        self.search_family = TextWithLabel(self.win, "Family", 300, 270)
-        self.search_family.text_box.bind("<KeyRelease>", self.find_by_family)
 
-        self.table = Table(self.win,
-                      ["Id", "Name", "Family"],
-                      [60, 100, 100],
-                      250,
-                      20,
-                      self.select_row)
-
-        Button(self.win, text="Add", width=8, command=self.save_click).place(x=20, y=250)
-        Button(self.win, text="Edit", width=8, command=self.edit_click).place(x=100, y=250)
-        Button(self.win, text="Remove", width=8, command=self.remove_click).place(x=180, y=250)
+        Button(self.win, text="Edit", width=10, command=self.edit_click).place(x=80, y=200)
 
         self.reset_form()
 
