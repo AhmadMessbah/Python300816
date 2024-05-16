9906e8b0d1cfe5e516fee74085ec93dda46ae616
Messbah <Messbah.A@Gmail.com>
2024-05-16 15:25:34 +0330

    messbah

--- view/main_view.py
+++ view/main_view.py
@@ -13,23 +13,29 @@ from view.driving_license_view import DrivingLicenseView
 
 class MainView:
     def person_click(self):
-        PersonView(self.user)
-        self.win.destroy()
-        del self.win
+        person_view = PersonView(self.user)
+
     def user_click(self):
         user_view = UserView()
+
     def sim_click(self):
-        sim_view= SimCardView(None)
+        sim_view = SimCardView(None)
+
     def military_click(self):
         military_view = MilitaryView(None)
+
     def medical_click(self):
         medical_view = MedicalView(None)
+
     def product_click(self):
         product_view = ProductView(None)
+
     def financial_click(self):
         financial_view = FinancialDocView(None)
+
     def lesson_click(self):
         lesson_view = LessonView(None)
+
     def driving_license_click(self):
         driving_license_view = DrivingLicenseView(None)
 
@@ -40,14 +46,23 @@ class MainView:
         self.win.title("Python App")
         Label(text=user.person.name + " " + user.person.family).place(x=20, y=20)
 
-        Button(self.win, text="Person",width=15 ,bg="lightblue", height=2, font=("Arial", 15), command=self.person_click).place(x=50, y=50)
-        Button(self.win, text="User",width=15 ,bg="lightblue", height=2, font=("Arial", 15), command=self.user_click).place(x=50, y=120)
-        Button(self.win, text="SimCard",width=15 ,bg="lightblue", height=2, font=("Arial", 15), command=self.sim_click).place(x=50, y=190)
-        Button(self.win, text="Military",width=15 ,bg="lightblue", height=2, font=("Arial", 15), command=self.military_click).place(x=50, y=260)
-        Button(self.win, text="Medical",width=15 ,bg="lightblue", height=2, font=("Arial", 15), command=self.medical_click).place(x=50, y=330)
-        Button(self.win, text="Product",width=15 ,bg="lightblue", height=2, font=("Arial", 15), command=self.product_click).place(x=50, y=400)
-        Button(self.win, text="Financial",width=15 ,bg="lightblue", height=2, font=("Arial", 15), command=self.financial_click).place(x=50, y=470)
-        Button(self.win, text="Lesson",width=15 ,bg="lightblue", height=2, font=("Arial", 15), command=self.lesson_click).place(x=50, y=540)
-        Button(self.win, text="DrivingLicense",width=15 ,bg="lightblue", height=2, font=("Arial", 15), command=self.driving_license_click).place(x=50, y=610)
+        Button(self.win, text="Person", width=15, bg="lightblue", height=2, font=("Arial", 15),
+               command=self.person_click).place(x=50, y=50)
+        Button(self.win, text="User", width=15, bg="lightblue", height=2, font=("Arial", 15),
+               command=self.user_click).place(x=50, y=120)
+        Button(self.win, text="SimCard", width=15, bg="lightblue", height=2, font=("Arial", 15),
+               command=self.sim_click).place(x=50, y=190)
+        Button(self.win, text="Military", width=15, bg="lightblue", height=2, font=("Arial", 15),
+               command=self.military_click).place(x=50, y=260)
+        Button(self.win, text="Medical", width=15, bg="lightblue", height=2, font=("Arial", 15),
+               command=self.medical_click).place(x=50, y=330)
+        Button(self.win, text="Product", width=15, bg="lightblue", height=2, font=("Arial", 15),
+               command=self.product_click).place(x=50, y=400)
+        Button(self.win, text="Financial", width=15, bg="lightblue", height=2, font=("Arial", 15),
+               command=self.financial_click).place(x=50, y=470)
+        Button(self.win, text="Lesson", width=15, bg="lightblue", height=2, font=("Arial", 15),
+               command=self.lesson_click).place(x=50, y=540)
+        Button(self.win, text="DrivingLicense", width=15, bg="lightblue", height=2, font=("Arial", 15),
+               command=self.driving_license_click).place(x=50, y=610)
 
         self.win.mainloop()
--- view/person_view.py
+++ view/person_view.py
@@ -52,7 +52,7 @@ class PersonView:
     def __init__(self, user):
         self.user = user
         # todo : label is on previous form
-        # Label(text=user.person.name + " " + user.person.family).place(x=0, y=0)
+        Label(text=user.person.name + " " + user.person.family).place(x=0, y=0)
         self.win = Tk()
         self.win.geometry("600x300")
         self.win.title("Person")
