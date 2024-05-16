f04fd6194889bfe713bcc7f292f48476ef612dc8
Messbah <Messbah.A@Gmail.com>
2024-05-16 17:22:52 +0330

    military - messbah

---
+++ controller/military_controller.py
@@ -0,0 +1,82 @@
+from model.da.military_da import MilitaryDa
+from model.da.person_da import PersonDa
+from model.entity.military import Military
+from model.tools.decorators import exception_handling
+
+
+class MilitaryController:
+    military_da = MilitaryDa()
+    person_da = PersonDa()
+
+    @classmethod
+    @exception_handling
+    def save(cls, serial_number, city, organ, start_year, start_month, start_day, end_year, end_month, end_day, soldier_id):
+        if soldier_id:
+            soldier = cls.person_da.find_by_id(int(soldier_id))
+            military = Military(serial_number,
+                                city,
+                                organ,
+                                (int(start_year), int(start_month), int(start_day)),
+                                (int(end_year), int(end_month), int(end_day)),
+                                soldier)
+        else:
+            military = Military(serial_number,
+                                city,
+                                organ,
+                                (int(start_year), int(start_month), int(start_day)),
+                                (int(end_year), int(end_month), int(end_day)))
+        cls.military_da.save(military)
+        return True, f"Record saved successfully {military}"
+
+    @classmethod
+    @exception_handling
+    def edit(cls, military_id, serial_number, city, organ,start_year, start_month, start_day, end_year, end_month, end_day, soldier_id):
+        #Person-Connect
+        soldier = cls.person_da.find_by_id(int(soldier_id))
+        military = Military(serial_number,
+                            city,
+                            organ,
+                            (int(start_year), int(start_month), int(start_day)),
+                            (int(end_year), int(end_month), int(end_day)),
+                            int(soldier))
+        military.military_id = military_id
+        old_military = cls.military_da.find_by_military_id(military_id)
+        cls.military_da.edit(military)
+        return True, (f"Record edited successfullyFrom : {old_military}\nTo: {military}")
+
+    @classmethod
+    @exception_handling
+    def remove(cls, person_id):
+        military = cls.military_da.find_by_military_id(person_id)
+        cls.military_da.remove(person_id)
+        return True, f"Record removed successfully {military}"
+
+    @classmethod
+    @exception_handling
+    def find_all(cls):
+        return True, cls.military_da.find_all(cls.person_da)
+
+    @classmethod
+    @exception_handling
+    def find_by_military_id(cls, military_id):
+        return True, cls.military_da.find_by_military_id(int(military_id))
+
+    @classmethod
+    @exception_handling
+    def find_by_serial_number(cls, serial_number):
+        return True, cls.military_da.find_by_serial_number(serial_number)
+
+    @classmethod
+    @exception_handling
+    def find_by_organ(cls, organ):
+        return True, cls.military_da.find_by_organ(organ)
+
+    @classmethod
+    @exception_handling
+    def find_by_city(cls, city):
+        return True, cls.military_da.find_by_city(city)
+
+    def find_by_soldier_id(cls, soldier_id, person_da=PersonDa()):
+        return True, cls.military_da.find_by_soldier_id(soldier_id)
+
+
---
+++ view/military_view.py
@@ -0,0 +1,173 @@
+from tkinter import *
+import tkinter.messagebox as msg
+from controller.military_controller import MilitaryController
+from view.component.label_text import TextWithLabel
+from view.component.table import Table
+from view.main_view import MainView
+
+
+class MilitaryView:
+
+    def reset_form(self):
+        self.id.variable.set("")
+        self.serial_number.variable.set("")
+        self.city.variable.set("")
+        self.organ.variable.set("")
+        self.start_year.variable.set("")
+        self.start_month.variable.set("")
+        self.start_day.variable.set("")
+        self.end_year.variable.set("")
+        self.end_month.variable.set("")
+        self.end_day.variable.set("")
+        self.soldier_id.variable.set(self.user.person.person_id)
+        status, military_list = MilitaryController.find_all()
+        if status:
+            self.table.refresh_table(military_list)
+
+
+    def select_row(self, military):
+        self.id.variable.set(military[0])
+        self.serial_number.variable.set(str("{:011d}".format(military[1])))
+        self.city.variable.set(military[2])
+        self.organ.variable.set(military[3])
+
+        self.start_year.variable.set(int(military[4][0:4]))
+        self.start_month.variable.set(int(military[4][5:7]))
+        self.start_day.variable.set(int(military[4][8:]))
+
+        self.end_year.variable.set(int(military[5][0:4]))
+        self.end_month.variable.set(int(military[5][5:7]))
+        self.end_day.variable.set(int(military[5][8:]))
+
+        self.soldier_id.variable.set(int(military[6][14:15]))
+
+    def save_click(self):
+        status, message = MilitaryController.save(self.serial_number.variable.get(),
+                                                  self.city.variable.get(),
+                                                  self.organ.variable.get(),
+                                                  self.start_year.variable.get(),
+                                                  self.start_month.variable.get(),
+                                                  self.start_day.variable.get(),
+                                                  self.end_year.variable.get(),
+                                                  self.end_month.variable.get(),
+                                                  self.end_day.variable.get(),
+                                                  self.soldier_id.variable.get())
+        if status:
+            msg.showinfo("Save Record", "Record Saved")
+            self.reset_form()
+        else:
+            msg.showerror("Save Error", message)
+
+    def edit_click(self):
+        status, message = MilitaryController.edit(self.id.variable.get(),
+                                                  self.serial_number.variable.get(),
+                                                  self.city.variable.get(),
+                                                  self.organ.variable.get(),
+                                                  self.start_year.variable.get(),
+                                                  self.start_month.variable.get(),
+                                                  self.start_day.variable.get(),
+                                                  self.end_year.variable.get(),
+                                                  self.end_month.variable.get(),
+                                                  self.end_day.variable.get(),
+                                                  self.soldier_id.variable.get())
+        if status:
+            msg.showinfo("Edit Record", "Record Edited")
+            self.reset_form()
+        else:
+            msg.showerror("Edit Error", message)
+
+    def remove_click(self):
+        status, message = MilitaryController.remove(self.id.variable.get())
+        if status:
+            msg.showinfo("Remove Record", "Person Removed")
+            self.reset_form()
+        else:
+            msg.showerror("Remove Error", message)
+
+    def find_by_city(self, event):
+        status, military_list = MilitaryController.find_by_city(self.search_city.variable.get())
+        if status:
+            self.table.refresh_table(military_list)
+
+    def find_by_organ(self, event):
+        status, military_list = MilitaryController.find_by_organ(self.search_organ.variable.get())
+        if status:
+            self.table.refresh_table(military_list)
+
+    def find_by_serial_number(self, event):
+        status, military_list = MilitaryController.find_by_serial_number(self.search_serial_number.variable.get())
+        if status:
+            self.table.refresh_table(military_list)
+
+    def find_by_id(self, event):
+        if self.search_id.variable.get():
+            status, military_list = MilitaryController.find_by_military_id(self.search_id.variable.get())
+            if status:
+                self.table.refresh_table([military_list])
+        else:
+            self.table.refresh_table(MilitaryController.find_all()[1])
+
+    def close_win(self):
+        self.win.destroy()
+        main_view = MainView(self.user)
+
+    def __init__(self, user):
+        self.user = user
+        self.win = Tk()
+
+        Label(text=user.person.name + " " + user.person.family).place(x=0, y=0)
+        self.win.protocol("WM_DELETE_WINDOW", self.close_win)
+
+
+        self.win.title("MilitaryRecord")
+        self.win.resizable(width=False, height=False)
+
+        # CENTER FORM
+        x = (self.win.winfo_screenwidth() - 1165) // 2
+        y = (self.win.winfo_screenheight() - 300) // 2
+        self.win.geometry(f"1165x300+{x}+{y}")
+
+        # WIDGETS
+        self.soldier_id = TextWithLabel(self.win, "Soldier ID", 20, 20, width=6, disabled=True)
+        self.id = TextWithLabel(self.win, "ID", 140, 20, disabled=True, distance=25, width=6)
+        self.serial_number = TextWithLabel(self.win, "Serial", 20, 60)
+        self.city = TextWithLabel(self.win, "City", 20, 100)
+        self.organ = TextWithLabel(self.win, "Organ", 20, 140)
+
+        # SEARCH
+        self.search_city = TextWithLabel(self.win, "Find By City", 250, 260, distance=80, width=13)
+        self.search_city.text_box.bind("<KeyRelease>", self.find_by_city)
+        self.search_organ = TextWithLabel(self.win, "Find By Organ", 425, 260, distance=90, width=13)
+        self.search_organ.text_box.bind("<KeyRelease>", self.find_by_organ)
+        self.search_serial_number = TextWithLabel(self.win, "Find by Serial", 610, 260, distance=85, width=13)
+        self.search_serial_number.text_box.bind("<KeyRelease>", self.find_by_serial_number)
+
+        self.search_id = TextWithLabel(self.win, "Search ID", 1035, 260, distance=63, width=5)
+        self.search_id.text_box.bind("<KeyRelease>", self.find_by_id)
+
+        # START DATE
+        self.start_year = TextWithLabel(self.win, "Start Date", 20, 180, disabled=False, width=8)
+        self.start_month = TextWithLabel(self.win, "/", 130, 180, 12, disabled=False, width=4)
+        self.start_day = TextWithLabel(self.win, "/", 165, 180, 12, disabled=False, width=4)
+
+        # END DATE
+        self.end_year = TextWithLabel(self.win, "End Date", 20, 220, disabled=False, width=8)
+        self.end_month = TextWithLabel(self.win, "/", 130, 220, 12, disabled=False, width=4)
+        self.end_day = TextWithLabel(self.win, "/", 165, 220, 12, disabled=False, width=4)
+
+        self.table = Table(self.win,
+                           ["ID", "Serial Number", "City", "Organ", "Start Date", "End Date", "Soldier"],
+                           [60, 100, 100, 100, 100, 100, 320],
+                           250,
+                           20,
+                           self.select_row)
+
+        Button(self.win, text="Add", width=5, command=self.save_click, bg="#e2e2e2").place(x=15, y=260)
+        Button(self.win, text="Edit", width=5, command=self.edit_click, bg="#e2e2e2").place(x=70, y=260)
+        Button(self.win, text="Remove", width=7, command=self.remove_click, bg="#e2e2e2").place(x=125, y=260)
+        Button(self.win, text="♻️", width=2, command=self.reset_form, bg="#e2e2e2").place(x=193, y=260)
+
+        self.reset_form()
+
+        self.win.mainloop()
+
