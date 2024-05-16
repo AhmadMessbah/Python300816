6e6457e00df690c3c3af8a2c20646945afd46eb1
raminfaryad <r.faryad@pinket.com>
2024-05-16 13:33:11 +0330

    update driving license contorller and add new method to find by person

--- controller/driving_license_controller.py
+++ controller/driving_license_controller.py
@@ -1,68 +1,60 @@
 from model.da.driving_license_da import DrivingLicenseDa
+from model.da.person_da import PersonDa
 from model.entity.driving_license import DrivingLicense
-from model.tools.validator import Validator
+from model.tools.decorators import exception_handling
 
 
 class DrivingLicenseController:
-    def __init__(self):
-        self.validator = Validator()
+    driving_license_da = DrivingLicenseDa()
+    person_da = PersonDa()
 
-    def save(self, serial_number, date ,city , expire_date):
-        try:
-            drivingLicense = DrivingLicense(
-                self.validator.name_validator(serial_number, "Invalid serial number"),
-                date,
-                self.validator.name_validator(city, "Invalid city"),
-                expire_date
-            )
-            licenceDa = DrivingLicenseDa()
-            licenceDa.save(drivingLicense)
-            return True, f"DrivingLicense saved successfully\n{drivingLicense}"
-        except Exception as e:
-            return False, str(e)
+    @classmethod
+    @exception_handling
+    def save(cls, serial_number, date ,city , expire_date, person_id):
+        if(person_id):
+            person = cls.person_da.find_by_id(person_id)
+            dl = DrivingLicense(serial_number, date, city, expire_date, person)
+        else:
+            dl = DrivingLicense(serial_number, date, city, expire_date)
+        
+        cls.driving_license_da.save(dl)
+        return True, f"Driving License saved successfully\n{dl}"
 
-    def edit(self, license_id, serial_number, date ,city , expire_date):
-        try:
-            drivingLicense = DrivingLicense(
-                self.validator.name_validator(serial_number, "Invalid serial number"),
-                date,
-                self.validator.name_validator(city, "Invalid city"),
-                expire_date
-            )
-            drivingLicense.id = license_id
-            licenceDa = DrivingLicenseDa()
-            licenceDa.edit(drivingLicense)
-            return True, (f"DrivingLicense edited successfully")
-        except Exception as e:
-            return False, str(e)
+    @classmethod
+    @exception_handling
+    def edit(cls, license_id, serial_number, date ,city , expire_date, person_id):
+        person = cls.person_da.find_by_id(person_id)
+        dl = DrivingLicense(serial_number, date, city, expire_date, person)
+        dl.id = license_id
+        old_dl = cls.driving_license_da.find_by_id(license_id)
+        cls.driving_license_da.edit(dl)
+        return True, (f"DrivingLicense edited successfully\nFrom : {old_dl}\nTo: {dl}")
 
-    def remove(self, licenseId):
-        try:
-            licenceDa = DrivingLicenseDa()
-            licenceDa.remove(licenseId)
-            return True, f"DrivingLicense removed successfully"
-        except Exception as e:
-            return False, str(e)
 
-    def find_all(self):
-        try:
-            licenceDa = DrivingLicenseDa()
-            return True, licenceDa.find_all()
-        except Exception as e:
-            return False, str(e)
-
-    def find_by_id(self, licenseId):
-        try:
-            licenceDa = DrivingLicenseDa()
-            return True, licenceDa.find_by_id(licenseId)
-        except Exception as e:
-            return False, str(e)
-
-    def find_by_serial_number(self, serialNumber):
-        try:
-            licenceDa = DrivingLicenseDa()
-            return True, licenceDa.find_by_serial_number(
-                self.validator.name_validator(serialNumber, "Invalid Serial Number")
-            )
-        except Exception as e:
-            return False, str(e)
+    @classmethod
+    @exception_handling
+    def remove(cls, license_id):
+        dl = cls.driving_license_da.find_by_id(license_id)
+        cls.driving_license_da.remove(license_id)
+        return True, f"DrivindLicense removed successfully\n{dl}"
+    
+    @classmethod
+    @exception_handling
+    def find_all(cls):
+        return True, cls.driving_license_da.find_all()
+    
+    @classmethod
+    @exception_handling
+    def find_by_id(cls, license_id):
+        return True, cls.driving_license_da.find_by_id(license_id)
+    
+    
+    @classmethod
+    @exception_handling
+    def find_by_seridal_number(cls, serial_number):
+        return True, cls.driving_license_da.find_by_serial_number(serial_number)
+    
+    @classmethod
+    @exception_handling
+    def find_by_person_id(cls, person_id):
+        return True, cls.driving_license_da.find_by_person_id(person_id)
