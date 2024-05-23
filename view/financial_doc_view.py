d5c4925cf17394632fc5297653539facb2077582
mohamadrezanikmaram <mohamadrezanikmaram7@gmail.com>
2024-05-23 16:30:08 +0330

    financial view updated

--- view/financial_doc_view.py
+++ view/financial_doc_view.py
@@ -1,15 +1,16 @@
 from tkinter import *
 import tkinter.messagebox as msg
+import tkinter.ttk as ttk
 
 from controller.financial_doc_controller import FinancialDocController
 from view.component.label_text import TextWithLabel
 from view.component.table import Table
 
+
 class FinancialDocView:
     def reset_form(self):
         self.id.variable.set("")
         self.amount.variable.set("")
-        self.doc_type.variable.set("")
         self.description.variable.set("")
         status, financial_list = FinancialDocController.find_all()
         if status:
@@ -23,7 +24,7 @@ class FinancialDocView:
         self.description.variable.set(financial_doc[4])
 
     def save_click(self):
-        status, message = FinancialDocController.save(self.amount.variable.get(),
+        status, message = FinancialDocController.save(int(self.amount.variable.get()),
                                                       self.doc_type.variable.get(),
                                                       self.description.variable.get(),
                                                       self.person_id.variable.get())
@@ -35,9 +36,9 @@ class FinancialDocView:
 
     def edit_click(self):
         status, message = FinancialDocController.edit(self.id.variable.get(),
-                                                self.amount.variable.get(),
-                                                self.doc_type.variable.get(),
-                                                self.description.variable.get())
+                                                      int(self.amount.variable.get()),
+                                                      self.doc_type.variable.get(),
+                                                      self.description.variable.get())
         if status:
             msg.showinfo("Edit Document", "Document Edited")
             self.reset_form()
@@ -52,43 +53,45 @@ class FinancialDocView:
         else:
             msg.showerror("Remove Error", message)
 
-    def find_by_date(self,event):
+    def find_by_date(self, event):
         status, financial_list = FinancialDocController.find_by_date(self.search_date_time.variable.get())
         if status:
             self.table.refresh_table(financial_list)
 
-
     # def close_win(self):
-         #self.win.destroy()
-        # main_view = MainView(self.person)
+    # self.win.destroy()
+    # main_view = MainView(self.person)
 
     def __init__(self, user):
         self.user = user
         win = Tk()
-        win.geometry("800x300")
+        win.geometry("800x325")
         win.title("Financial Document")
 
-        self.id = TextWithLabel(win, "  Id:", 20, 20, disabled=True)
-        self.amount = TextWithLabel(win, "amount:", 20, 60)
-        self.search_date_time  = TextWithLabel(win, "date time:", 300, 270)
+        self.id = TextWithLabel(win, "  Id", 20, 20, disabled=True)
+        self.amount = TextWithLabel(win, "Amount", 20, 60)
+        Label(win, text="Doc Type").place(x=20, y=100)
+        self.doc_type = ttk.Combobox(win, values=["income", "outcome"],state="readonly")
+        self.doc_type.place(x=80, y=100, width=125)
+        self.doc_type.current(0)
+        self.description = TextWithLabel(win, "Describe:", 20, 140)
+        self.person_id = TextWithLabel(win, "Person Id", 20, 180, disabled=True)
+        self.person_id.variable.set(self.user.person.person_id)
+        self.search_date_time = TextWithLabel(win, "date time:", 300, 270)
         self.search_date_time.text_box.bind("<KeyRelease>", self.find_by_date)
-        self.doc_type = TextWithLabel(win, "doc type:", 20, 110)
-        self.description = TextWithLabel(win, "description:", 20, 150)
-        self.person_id = TextWithLabel(win, "Person Id", 20, 190, disabled=True)
-        self.person_id.variable.set( self.user.person.person_id)
 
         self.table = Table(win,
-                      ["Id", "amount", "date_time", "doc_type", "description"],
-                      [50, 100, 100,100,150],
-                      250,
-                      20,
-                      self.select_row)
+                           ["Id", "amount", "date_time", "doc_type", "description"],
+                           [50, 100, 100, 100, 150],
+                           250,
+                           20,
+                           self.select_row)
 
-        Button(win, text="Add", width=8, command=self.save_click).place(x=20, y=250)
-        Button(win, text="Edit", width=8, command=self.edit_click).place(x=100, y=250)
-        Button(win, text="Remove", width=8, command=self.remove_click).place(x=180, y=250)
+        Button(win, text="New", width=8, command=self.reset_form(), bg="lightgreen").place(x=35, y=240)
+        Button(win, text="Save", width=8, command=self.save_click).place(x=140, y=240)
+        Button(win, text="Edit", width=8, command=self.edit_click).place(x=35, y=280)
+        Button(win, text="Remove", width=8, command=self.remove_click, bg="red").place(x=140, y=280)
 
         self.reset_form()
 
         win.mainloop()
-
