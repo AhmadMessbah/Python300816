0c8cc1c6143679b1a10f98b03f1f50c6bd24d5af
mohamadrezanikmaram <mohamadrezanikmaram7@gmail.com>
2024-05-23 19:12:29 +0330

    fin_view updated

--- view/financial_doc_view.py
+++ view/financial_doc_view.py
@@ -35,7 +35,6 @@ class FinancialDocView:
         else:
             msg.showerror("Save Error", message)
 
-
     def edit_click(self):
         status, message = FinancialDocController.edit(self.id.variable.get(),
                                                       int(self.amount.variable.get()),
@@ -61,8 +60,8 @@ class FinancialDocView:
             self.table.refresh_table(financial_list)
 
     def close_win(self):
-       self.win.destroy()
-       main_view = MainView(self.person)
+        self.win.destroy()
+        main_view = MainView(self.user)
 
     def __init__(self, user):
         self.user = user
@@ -73,27 +72,27 @@ class FinancialDocView:
         self.id = TextWithLabel(win, "  Id", 20, 20, disabled=True)
         self.amount = TextWithLabel(win, "Amount", 20, 60)
         Label(win, text="Doc Type").place(x=20, y=100)
-        self.doc_type = ttk.Combobox(win, values=["income", "outcome"],state="readonly")
+        self.doc_type = ttk.Combobox(win, values=["Income", "Outcome"], state="readonly")
         self.doc_type.place(x=80, y=100, width=125)
         self.doc_type.current(0)
-        self.description = TextWithLabel(win, "Describe:", 20, 140)
+        self.description = TextWithLabel(win, "Describe", 20, 140)
         self.person_id = TextWithLabel(win, "Person", 20, 180, disabled=True)
         self.person_id.variable.set(f"{self.user.person.person_id} - {self.user.person.name} {self.user.person.family}")
-        self.date_time = TextWithLabel(win, "Date", 20, 220)
-        self.search_date_time = TextWithLabel(win, "date time:", 300, 270,disabled=True)
+        self.date_time = TextWithLabel(win, "Date", 20, 220, disabled=True)
+        self.search_date_time = TextWithLabel(win, "Date time:", 300, 270)
         self.search_date_time.text_box.bind("<KeyRelease>", self.find_by_date)
 
         self.table = Table(win,
-                           ["Id", "amount", "date_time", "doc_type", "description"],
+                           ["Id", "Amount", "Date", "Doc Type", "Describe"],
                            [50, 100, 100, 100, 150],
                            250,
                            20,
                            self.select_row)
 
-        Button(win, text="New", width=8, command=self.reset_form, bg="lightgreen").place(x=35, y=240)
-        Button(win, text="Save", width=8, command=self.save_click).place(x=140, y=240)
-        Button(win, text="Edit", width=8, command=self.edit_click).place(x=35, y=280)
-        Button(win, text="Remove", width=8, command=self.remove_click, bg="red").place(x=140, y=280)
+        Button(win, text="New", width=8, command=self.reset_form, bg="lightgreen").place(x=35, y=250)
+        Button(win, text="Save", width=8, command=self.save_click).place(x=120, y=250)
+        Button(win, text="Edit", width=8, command=self.edit_click).place(x=35, y=290)
+        Button(win, text="Remove", width=8, command=self.remove_click, bg="red").place(x=120, y=290)
 
         self.reset_form()
 
